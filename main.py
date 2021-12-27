9c88be0bd961a68c60a715e1b1e3b02c7d0bf2ed
pumpkin <65254064+nadya-web@users.noreply.github.com>
2021-12-28 01:17:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,46 @@
+import webbrowser
+
 import matplotlib.pyplot as plt
-import numpy as np
-import scipy
 import sys
 import requests
 import astropy.units as u
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QWidget, QVBoxLayout, QPushButton, QLineEdit
-from astropy.coordinates import SkyCoord
-from astroquery.vizier import Vizier
-from astroquery.simbad import Simbad
-from astropy.stats import sigma_clipped_stats
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QWidget, QVBoxLayout, QPushButton, QLineEdit, QSizePolicy
 from astropy.io import fits
 from astropy import table
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-from photutils import DAOStarFinder
 from matplotlib.colors import LogNorm
 from astropy.wcs import WCS
 from bs4 import BeautifulSoup
 import PyQt5
-from PyQt5 import uic
+from PyQt5 import uic, QtGui
 
 
-class Window(QMainWindow):
+class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
-        uic.loadUi('design.ui', self)
+        uic.loadUi('win.ui', self)  # подгружаем дизайн
 
         self.error = Error()
 
-        self.contact_link = self.findChildren(QLabel)[2]
-        self.name = self.findChildren(QLineEdit)[0]
+        self.contacts = self.findChildren(QPushButton)[0]
+        self.contacts.clicked.connect(lambda: webbrowser.open('https://vk.com/id510788594'))
 
-        self.save_btn = self.findChildren(QPushButton)[0]
+        self.name = self.findChildren(QLineEdit)[0]
+        self.wait = self.findChildren(QLabel)[1]
+
+        self.save_btn = self.findChildren(QPushButton)[1]
         self.save_btn.clicked.connect(self.parsing)
 
-        self.layout = self.findChildren(QVBoxLayout)[0]
+
+        self.img_layout = self.findChildren(QVBoxLayout)[0]
+        self.lbl = QLabel(self)
+        self.pix = QtGui.QPixmap('cat2.jpg')
+        self.lbl.setPixmap(self.pix)
+        self.img_layout.addWidget(self.lbl)
+
 
     def parsing(self):
         galaxy_name = self.name.text()  # имя созвездия
-
-        if len(galaxy_name) > 3:
-            self.error.show()
-            return 0
 
         home_page = f'https://dr12.sdss.org/fields/name?name={galaxy_name}'
 
@@ -48,6 +48,11 @@ class Window(QMainWindow):
         soup = BeautifulSoup(html, 'html.parser')  # парсим данные с этой страницы
 
         dl = soup.find('dl', class_='dl-horizontal')  # ищем среди данных тег "DL"
+
+        if dl == None:  # Проверяем существует ли данные о запрошенной галактике
+            self.error.show()  # если нет, выводим ошибку
+            return 0
+
         dds = [dd for dd in dl.find_all('dd')]  # в полученых данных ищем все теги 'DD'
         links = [link for link in dds[1].find_all('a')]  # среди всех "DD" ищем теги с сылками
 
@@ -60,15 +65,14 @@ class Window(QMainWindow):
 
         self.plt_widget = FigureCanvasQTAgg(self.image_rendering())  # создаем виджет с картинкой
 
-        for i in reversed(range(self.layout.count())) :  # удаляем предыдущие картинки
-            self.layout.itemAt(i).widget().setParent(None)
+        for i in reversed(range(self.img_layout.count())) :  # удаляем предыдущие картинки
+            self.img_layout.itemAt(i).widget().setParent(None)
 
-        self.layout.addWidget(self.plt_widget)  # добавляем новую
+        self.img_layout.addWidget(self.plt_widget)  # добавляем новую
 
     def image_rendering(self):
         master_fits = fits.open('name.fits.bz2')[0]
         data = master_fits.data
-        print(data)
         wcs = WCS(master_fits.header)
 
         fig, ax = plt.subplots()
@@ -81,14 +85,17 @@ class Window(QMainWindow):
 class Error(QWidget):
     def __init__(self):
         super(Error, self).__init__()
-        self.setWindowTitle('ошибка!')
-        self.main_layout = QVBoxLayout(self)
-        self.text = QLabel('Invalid galaxy name', self)
-        self.main_layout.addWidget(self.text)
+        uic.loadUi('error.ui', self)
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle('Galaxy Visualisator')
+        self.setFixedSize(1341, 697)
+        self.setCentralWidget(Window())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = Window()
+    window = MainWindow()
     window.show()
     sys.exit(app.exec())
